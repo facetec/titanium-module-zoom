264f29f5c88051a77e69ccdc53e43527fe34d35b
Greg Perez <gperez@facetec.com>
2017-05-22 17:22:53 -0400

    Android: Work around bug in ‘respackage’

--- android/prepare.sh
+++ android/prepare.sh
@@ -10,10 +10,8 @@ AAR_PATH="$ANDROID_DIR/zoom-authentication.aar"
 
 LIB_DIR="$ANDROID_DIR/lib"
 ASSETS_DIR="$ANDROID_DIR/assets"
-JNI_DIR="$ANDROID_DIR/zoom-jni"
 
 rm -rf $LIB_DIR/* &&
-rm -rf $JNI_DIR/* &&
 rm -rf $ASSETS_DIR* &&
 rm -rf $ANDROID_DIR/libs/ &&
 rm -rf $ANDROID_DIR/platform/android/res/* &&
@@ -28,9 +26,13 @@ mv $LIB_DIR/classes.jar $LIB_DIR/zoom-authentication.jar &&
 # armeabi-v7a native libs
 mkdir -p $ANDROID_DIR/libs/ &&
 unzip -o $AAR_PATH jni/* -d $ANDROID_DIR/libs/ &&
+mv $ANDROID_DIR/libs/jni/* $ANDROID_DIR/libs/ &&
+rm -rf $ANDROID_DIR/libs/jni
 
 # Assets
 unzip -o $AAR_PATH assets/* -d $ASSETS_DIR/ &&
+mv $ANDROID_DIR/assets/assets/* $ANDROID_DIR/assets/ &&
+rm -rf $ANDROID_DIR/assets/assets
 
 # Resources
 unzip -o $AAR_PATH res/* -d $ANDROID_DIR/platform/android
